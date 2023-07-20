6d9c540f8d839cb9376320b5bd449188db1ce2f6
Vittorio <tonibusatta@gmail.com>
2023-07-20 23:34:29 +0400

    disable auto zoom

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata = {
   title: "Hestia - Rent your dream house",
   description: "Airbnb Clone",
   charset: "utf-8",
-  viewport: "width=device-width, initial-scale=1",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
 };
 
 export default async function RootLayout({
